4acfbd4f2688809e76bb676ef0359052b7d4e399
matias.mastrangelo <matias.mastrangelo@fing.com.uy>
2024-05-27 17:09:36 -0300

    Carga base

--- obl2-ray-traicing/CargaArchivo.cpp
+++ obl2-ray-traicing/CargaArchivo.cpp
@@ -1,1 +1,12 @@
 #include "CargaArchivo.h"
+#include "Esfera.h"
+#include "Objeto.h"
+#include <fstream>
+
+CargaArchivo::CargaArchivo()
+{
+	std::ifstream f("Especificaciones.json");
+	json data = json::parse(f);
+
+
+}
--- obl2-ray-traicing/CargaArchivo.h
+++ obl2-ray-traicing/CargaArchivo.h
@@ -1,5 +1,76 @@
 #pragma once
+
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
+struct Plano
+{
+	int ancho;
+	int largo;
+	int altura;
+	int r;
+	int g;
+	int b;
+};
+
+struct Rectangulo
+{
+	int ancho;
+	int largo;
+	int altura;
+	int x;
+	int y;
+	int z;
+	int r;
+	int g;
+	int b;
+	int Transparencia;
+	int Refleccion;
+	int Refraccion;
+};
+
+struct Esfera
+{
+	int radio;
+	int x;
+	int y;
+	int z;
+	int r;
+	int g;
+	int b;
+	int Transparencia;
+	int Refleccion;
+	int Refraccion;
+};
+
+struct Cilindro
+{
+	int radio;
+	int altura;
+	int x;
+	int y;
+	int z;
+	int r;
+	int g;
+	int b;
+	int Transparencia;
+	int Refleccion;
+	int Refraccion;
+};
+
+
 class CargaArchivo
 {
+	private:
+
+		json data;
+		std::vector<Plano> cuarto;
+		std::vector<Rectangulo> rects;
+		std::vector<Esfera> sphers;
+		std::vector<Cilindro> cilins;
+
+	public:
+
+		CargaArchivo();
 };
 
